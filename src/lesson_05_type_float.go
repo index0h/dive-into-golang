4b0872e9db379124ba836ca5fbd9fe667a281d97
Roman Levishchenko <index.0h@gmail.com>
2014-09-28 03:22:16 +0300

    add lesson_05_type_float

---
+++ src/lesson_05_type_float.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+func main() {
+	var defaultFloat float32
+	floatVar := 5.5
+
+	fmt.Println("defaultfloat       = ", defaultFloat, "\n")               // 0
+
+	fmt.Printf("floatVar (%s) = %v\n", reflect.TypeOf(floatVar), floatVar) // 5.5
+
+	fmt.Println("\t------------------------------------------------------------------\n\n")
+
+	fmt.Println("MAX float32        = ", math.MaxFloat32)
+	fmt.Println("MIN float32        = ", math.SmallestNonzeroFloat32, "\n")
+
+	fmt.Println("MAX float64        = ", math.MaxFloat64)
+	fmt.Println("MIN float64        = ", math.SmallestNonzeroFloat64, "\n")
+}
