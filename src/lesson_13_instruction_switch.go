9bafe405494fc6581a38660453088de2ca7d8628
Roman Levishchenko <index.0h@gmail.com>
2014-10-21 10:13:07 +0300

    add lesson_13_instruction_switch.go

---
+++ src/lesson_13_instruction_switch.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func getSuffix() string {
+	return "jpg"
+}
+
+func main() {
+
+	fmt.Print("First:  ")
+	switch {
+	case 1 < 2:
+		fmt.Println("1 < 2") // First:  TRUE  1 < 2
+	case 2 > 3:
+		fmt.Println("2 > 3")
+	}
+
+	fmt.Print("Second: ")
+	switch suffix := getSuffix(); suffix {
+	case "png":
+		fallthrough
+	case "jpg":
+		fallthrough
+	case "gif":
+		fmt.Println("Image") // Second: Image
+	default:
+		fmt.Println("Not image")
+	}
+
+	var someObject interface{}
+
+	fmt.Print("Third:  ")
+	switch someObject.(type) {
+	case bool:
+		fmt.Println("bool")
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	default:
+		fmt.Println("Sorry, I don't know") // Third:  Sorry, I don't know
+	}
+
+}
