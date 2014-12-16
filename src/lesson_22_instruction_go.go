1803ba49e5fdf1f47a2eb3cc0217fcf6a694300b
Roman Levishchenko <index.0h@gmail.com>
2014-12-16 22:48:02 +0200

    add lesson_22_instruction_go

---
+++ src/lesson_22_instruction_go.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"sync"
+	"strconv"
+)
+
+// Current function works 1 second
+func Work(name string) {
+	fmt.Printf("Work: %-5v start\n", name)
+	time.Sleep(time.Second)
+	fmt.Printf("Work: %-5v end\n", name)
+}
+
+// Simple demonstration of goroutine
+func Sleep() {
+	go Work("simple")
+	time.Sleep(2 * time.Second)
+}
+
+// Demonstration of sync.WaitGroup
+func WaitGroup() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+
+		wg.Add(1)
+
+		go func(id int) {
+
+			defer wg.Done()
+
+			Work("wg-" + strconv.Itoa(id))
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	fmt.Println("main: start")
+
+	Sleep()
+
+	WaitGroup()
+
+	fmt.Println("main: end")
+}
