8a9ed349b3003f4281fa2b7f4d346e35c2e32f26
Roman Levishchenko <index.0h@gmail.com>
2015-02-02 23:22:49 +0200

    fix bug in lesson

--- src/lesson_08_type_silce.go
+++ src/lesson_08_type_silce.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Printf("first  len(%d) cap(%d) = %v\n", len(first), cap(first),  first)   // first  len(5) cap(15) = [0 0 0 0 0]
 	fmt.Printf("second len(%d) cap(%d) = %v\n", len(second), cap(second), second) // second len(7) cap(3)  = [0 0 0]
 	fmt.Printf("third  len(%d) cap(%d) = %v\n", len(third), cap(third),  third)   // third  len(0) cap(0)  = []
-	fmt.Printf("fourth len(%d) cap(%d) = %v\n", len(fourth), cap(fourth), fourth) // fourth len(6) cap(6) = [1 2 3 4 5 6]
+	fmt.Printf("fourth len(%d) cap(%d) = %v\n", len(fourth), cap(fourth), fourth) // fourth len(6) cap(6)  = [1 2 3 4 5 6]
 
 	fmt.Println("\n------------------------------------------------------------------\n")
 
